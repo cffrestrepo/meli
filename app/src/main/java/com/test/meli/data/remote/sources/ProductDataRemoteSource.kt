283f7000a7020ddd89a8f72cc9e2d2ca0cd4bec2
cristian fuentes <cff.restrepo@hotmail.com>
2023-05-17 15:18:19 -0500

    (Either): implementing either

--- app/src/main/java/com/test/meli/data/remote/sources/ProductDataRemoteSource.kt
+++ app/src/main/java/com/test/meli/data/remote/sources/ProductDataRemoteSource.kt
@@ -1,6 +1,10 @@
 package com.test.meli.data.remote.sources
 
+import com.test.meli.commons.Either
+import com.test.meli.data.network.HandledError
+import com.test.meli.data.remote.response.LookFor
+
 interface ProductDataRemoteSource {
 
-    fun getProductsBySearch()
+    suspend fun getProductsBySearch(): Either<HandledError, LookFor?>
 }
