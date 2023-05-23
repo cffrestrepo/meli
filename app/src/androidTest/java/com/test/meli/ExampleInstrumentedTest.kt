9bd297a5f49321058eb88c9dace1193173573c8c
cristian fuentes <cff.restrepo@hotmail.com>
2023-05-22 20:28:52 -0500

    (UnitTestUseCase): implementation of tests for use cases

--- app/src/androidTest/java/com/test/meli/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/test/meli/ExampleInstrumentedTest.kt
@@ -8,10 +8,11 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
-/**
+/*
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
+ */
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
@@ -22,4 +23,3 @@ class ExampleInstrumentedTest {
         assertEquals("com.test.meli", appContext.packageName)
     }
 }
- */
